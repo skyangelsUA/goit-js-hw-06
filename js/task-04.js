bda4dab21af0c8e4145f9b50a198c131d022a157
Dima KIM <dkskyangels@gmail.com>
2022-02-09 10:13:59 +0200

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,15 +1,17 @@
 const decrimentBtn = document.querySelector("[data-action='decrement']");
 const inrementBtn = document.querySelector("[data-action='increment']");
 
-let counterValue = 0;
+let counterValue = document.querySelector('#value');;
+
+
 
 const increment = () => {
-  counterValue += 1;
-  document.getElementById("value").textContent = counterValue;
+  counterValue.textContent = Number(counterValue.textContent) + 1;
+  
 };
 const decrement = () => {
-  counterValue -= 1;
-  document.getElementById("value").textContent = counterValue;
+  counterValue.textContent = Number(counterValue.textContent) - 1;
+  
 };
 
 inrementBtn.addEventListener("click", increment);
